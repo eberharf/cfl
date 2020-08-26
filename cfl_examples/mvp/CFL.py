0f40d07de2e42331690d580a57e356e9a19b7c4c
Jenna Kahn <jmkahn@g.hmc.edu>
2020-08-26 12:14:58 -0700

    added commenting to cluster.py and CFL.py. reorganized cluster.py so that getYs() would be applied regardless of clustering method chosen

--- cfl_examples/mvp/CFL.py
+++ cfl_examples/mvp/CFL.py
@@ -5,34 +5,52 @@ import visualization # visualize clusters created
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 
-TRAINING_DATA_SIZE = 0.85
-RANDOM_SEED = 42
-N_EPOCHS = 10
+TRAINING_DATA_SIZE = 0.85 #proportion of data to assign for training (vs testing)
+RANDOM_SEED = 42 #set random seed (for reproducibility)
+N_EPOCHS = 10 #number of epochs to train model on 
 
 class CFL():
 
-    def __init__(self, X, Y, training_data_size =TRAINING_DATA_SIZE, CDE='CondExp', clustering_method='KNN') :
+    def __init__(self, X, Y, training_data_size=TRAINING_DATA_SIZE, CDE='MDN', clustering_method='KNN') :
+        """
+        creates the CFL object
+
+        Parameters: 
+            X, Y = high dimensional, observational data sets 
+            training_data_size (float) = proportion of data to assign for training (vs testing)
+            CDE (str) = method to use for Conditional Density Estimation 
+            clustering_method (str) = method to use for partitioning data into obs. classes
+        """
+        
         # define training and test sets
         self.X = X
         self.Y = Y
         self.CDE = CDE 
         self.clustering_method = clustering_method
+        
+        #shuffles and splits data into training and testing sets
         self.X_tr, self.X_ts, self.Y_tr, self.Y_ts = \
             train_test_split(X, Y, shuffle=True, train_size=training_data_size, random_state=RANDOM_SEED)
-        self.normalizeData()
+
+        self.normalizeData() #normalize each chunk of data to have mean of 0 and var 1 
         
         # construct density estimation model
         # TODO: make it so people can rebuild the model 
         self.density_model = CondExp.CondExp(n_xfeatures=X.shape[1], n_yfeatures=Y.shape[1], verbose=True)
         
-    #TODO: 'normalize data' 
     def normalizeData(self): 
+        """
+        independently normalizes each training and testing set to have mean 0 and var 1. 
+        The scaling vector is calculated separately for training and testing so that there is 
+        no leak of information about the testing data into the training data
+        """
         self.X_tr = self.normalizeHelper(self.X_tr)
         self.X_ts = self.normalizeHelper(self.X_ts)
         self.Y_tr = self.normalizeHelper(self.Y_tr)
         self.Y_ts = self.normalizeHelper(self.Y_ts)
 
     def normalizeHelper(self, data):
+        """for a inputted data set, calculated the scaler to normalize the data and applies the transformation"""
         scaler = StandardScaler().fit(data)
         normalized = scaler.transform(data).astype('float32')
         return normalized
@@ -40,11 +58,11 @@ class CFL():
 
     #wrappers for CondExp methods 
     def train_model(self, n_epochs = N_EPOCHS): 
-        '''train the density estimation model''' 
+        """train the density estimation model""" 
         self.density_model.train_model(self.X_tr, self.Y_tr, self.X_ts, self.Y_ts, n_epochs=n_epochs, save_fname='net_params/net')
 
     def predict(self, data):
-        '''predicts (the expectation of) the P(Y|X=x) for a particular x using the density estimation model'''
+        """predicts (the expectation of) the P(Y|X=x) for a particular x using the density estimation model"""
         pred = self.density_model.predict(data)
         return pred
     
@@ -53,7 +71,7 @@ class CFL():
         return None
     
     def evaluate(self):
-        '''evaluate predictive performance of model'''
+        """evaluate predictive performance of model"""
         pass
 
 
@@ -69,7 +87,7 @@ class CFL():
 
     # TODO: get conditional probability
     def get_cond_prob(self):
-        '''Compute and print P(y_lbl | x_lbl)'''
+        """Compute and print P(y_lbl | x_lbl)"""
         P_CE = np.array([np.bincount(self.y_lbls.astype(int)[self.x_lbls==x_lbl], 
             minlength=self.y_lbls.max()+1).astype(float) for x_lbl in np.sort(np.unique(self.x_lbls))])
         P_CE = P_CE/P_CE.sum()
