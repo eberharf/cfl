330d960b43c0723b315a9e17141807d6cdb19ea0
Iman Wahle <imanwahle@gmail.com>
2020-10-16 11:44:18 -0700

    Clean up file structure

--- cfl/__init__.py
+++ cfl/__init__.py
@@ -1,6 +1,6 @@
 # this file is run when we import the cfl package
 import cfl.cfl as cfl 
-import cfl.visualization as visualization
+import cfl.visualization_methods as visualization_methods
 import cfl.cluster_methods as cluster_methods
 import cfl.density_estimation_methods as density_estimation_methods
 import cfl.core_cfl_objects as core_cfl_objects
