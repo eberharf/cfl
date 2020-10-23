2493fefd0b744f12957205764a0f56a537bc07c2
Jenna Kahn <jmkahn@g.hmc.edu>
2020-10-23 09:49:22 -0700

    add the saver module to the import path for cfl package

--- cfl/__init__.py
+++ cfl/__init__.py
@@ -5,3 +5,4 @@ import cfl.cluster_methods as cluster_methods
 import cfl.density_estimation_methods as density_estimation_methods
 import cfl.core_cfl_objects as core_cfl_objects
 import cfl.util as util
+import cfl.saver 
