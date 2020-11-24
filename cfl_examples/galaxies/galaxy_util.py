ddb1fa0c45758900da4dee03d947191a8279b638
Iman Wahle <imanwahle@gmail.com>
2020-11-24 11:49:57 -0800

    Package data for Eric

--- cfl_examples/galaxies/galaxy_util.py
+++ cfl_examples/galaxies/galaxy_util.py
@@ -7,6 +7,8 @@ be incorporated into cfl util code.
 '''
 
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
 
 def reshape_input(vec, im_no):
     if im_no==0:
@@ -29,7 +31,48 @@ def calculate_arp(image): # arp = average radial profile = average
     # calculate the mean
     eps = 0.5
     f = lambda r : image[(R >= r-eps) & (R < r+eps)].mean()
-    r  = np.linspace(0.1,100,num=300)
+    r  = np.linspace(0.1,40,num=200)
     mean = np.vectorize(f)(r)
 
     return r,mean
+
+
+
+def multiline(xs, ys, c, ax=None, **kwargs):
+    """Plot lines with different colorings
+
+    Parameters
+    ----------
+    xs : iterable container of x coordinates
+    ys : iterable container of y coordinates
+    c : iterable container of numbers mapped to colormap
+    ax (optional): Axes to plot on.
+    kwargs (optional): passed to LineCollection
+
+    Notes:
+        len(xs) == len(ys) == len(c) is the number of line segments
+        len(xs[i]) == len(ys[i]) is the number of points for each line (indexed by i)
+
+    Returns
+    -------
+    lc : LineCollection instance.
+    """
+
+    # find axes
+    ax = plt.gca() if ax is None else ax
+
+    # create LineCollection
+    segments = [np.column_stack([x, y]) for x, y in zip(xs, ys)]
+    lc = LineCollection(segments, **kwargs)
+
+    # set coloring of line segments
+    #    Note: I get an error if I pass c as a list here... not sure why.
+    lc.set_array(np.asarray(c))
+
+    # add lines to axes and rescale 
+    #    Note: adding a collection doesn't autoscalee xlim/ylim
+    ax.add_collection(lc)
+    # ax.set_xscale("log")
+    # ax.set_yscale("log")
+    ax.autoscale()
+    return lc
