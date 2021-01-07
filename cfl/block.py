4b5294cd6137c38e6ccfc0b23d5ec642ae16b9f9
Jenna Kahn <jmkahn@g.hmc.edu>
2021-01-07 14:58:03 -0500

    add an abstractmethod tag to save_block()

--- cfl/block.py
+++ cfl/block.py
@@ -6,27 +6,27 @@ class Block(metaclass=ABCMeta):
     A Block is an object that can be trained and that can:
         1) be trained on a Dataset
         2) predict some target for a Dataset.
-    Blocks are intended to be components of a graph workflow in an Experiment. 
-    For example, if the graph Block_A->Block_B is constructed in an Experiment, 
-    the output of Block_A.predict will provide input to Block_B.predict. 
+    Blocks are intended to be components of a graph workflow in an Experiment.
+    For example, if the graph Block_A->Block_B is constructed in an Experiment,
+    the output of Block_A.predict will provide input to Block_B.predict.
     '''
 
-    
+
     def __init__(self, name, data_info, params):
         '''
-        Instantiate the specified model. 
+        Instantiate the specified model.
 
         Arguments:
             model_name : name of model (str)
             data_info : dict of information about associated datasets (dict)
             model_params : parameters for this model (dict)
-        
+
         Returns: None
         '''
         self.trained = False
         # TODO: check input validity
         self.data_info = data_info
-    
+
     @abstractmethod
     def load_block(self, path):
         '''
@@ -34,17 +34,18 @@ class Block(metaclass=ABCMeta):
 
         Arguments:
             path : path to load from
-        
+
         Returns: None
         '''
-    
+
+    @abstractmethod
     def save_block(self, path):
         '''
         Save a Block that has been trained so that in can be reconstructed
         using load_block.
 
         Arguments:
-            path : path to save at 
+            path : path to save at
 
         Returns: None
         '''
@@ -59,7 +60,7 @@ class Block(metaclass=ABCMeta):
             prev_results : any results needed from previous Block training (dict)
         '''
         pass
-    
+
     @abstractmethod
     def predict(self, dataset, prev_results=None):
         '''
@@ -76,14 +77,14 @@ class Block(metaclass=ABCMeta):
         Return name of model.
 
         Arguments: None
-        Returns: name (str)    
+        Returns: name (str)
         '''
         return self.name
 
     def is_trained(self):
         '''
         Return whether this block has been trained yet.
-        
+
         Arguments: None
         Returns: whether the block has been trained (bool)
         '''
