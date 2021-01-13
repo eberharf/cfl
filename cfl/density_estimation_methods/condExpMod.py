c959533900b150954e320f610aa818637b6b0379
Jenna Kahn <jmkahn@g.hmc.edu>
2021-01-13 09:19:09 -0500

    add specific details about mismatch to assertion statements

--- cfl/density_estimation_methods/condExpMod.py
+++ cfl/density_estimation_methods/condExpMod.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 from cfl.density_estimation_methods.condExpBase import CondExpBase
 
 class CondExpMod(CondExpBase):
-    ''' A child class of CondExpBase that takes in model specifications from 
-        self.params to define the model architecture. This class aims to 
+    ''' A child class of CondExpBase that takes in model specifications from
+        self.params to define the model architecture. This class aims to
         simplify the process of tuning a mainstream feed-forward model.
-    
-        See CondExpBase documentation for more details. 
-    
+
+        See CondExpBase documentation for more details.
+
     '''
     def __init__(self, name, data_info, params):
         ''' Initialize model and define network.
@@ -27,7 +27,7 @@ class CondExpMod(CondExpBase):
         ''' Define the neural network based on dimensions passed in during initialization.
             This model takes specifications through the self.params dict to define
             it's architecture.
-            
+
             Arguments: None
             Returns: the model (tf.keras.models.Model object)
         '''
@@ -36,14 +36,16 @@ class CondExpMod(CondExpBase):
         assert self.params['activations'] is not {}, "Please specify layer sizes in params['activations']."
         assert self.params['dropouts'] is not {}, "Please specify layer sizes in params['dropouts']."
         assert self.params['dense_units'][-1] == self.data_info['Y_dims'][1], \
-                "The output layer size (last entry in params['dense_units'] should be equal to the number of Y features."
-            
-        assert len(self.params['dense_units']) == len(self.params['activations']), \
-                "params['dense_units'] and params['activation'] should be the same length."
-        assert len(self.params['dense_units']) == len(self.params['dropouts']), \
-                "params['dense_units'] and params['activation'] should be the same length."
+                "The output layer size (last entry in params['dense_units'] should be equal to the number of Y features but instead is {}".format(self.params['dense_units'][-1])
 
-        
+        assert len(self.params['dense_units']) == len(self.params['activations']), \
+                "params['dense_units'] and params['activation'] should be the same length but instead are {} and {}.".format(self.params['dense_units'], self.params['activations'])
+        assert len(self.params['dense_units']) == len(self.params['dropouts']), \
+                "params['dense_units'] and params['dropouts'] should be the same length but instead are {} and {}.".format(self.params['dense_units'], self.params['dropouts'])
+
+
+
+
         arch = [tf.keras.layers.Input(shape=(self.data_info['X_dims'][1],))] # input layer
         for units,act,dropout in zip(self.params['dense_units'], self.params['activations'], self.params['dropouts']):
             arch.append(tf.keras.layers.Dense(units=units, activation=act))
