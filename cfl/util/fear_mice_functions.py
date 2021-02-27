831a45489f95830453b70d91e563f120fc24d6c0
Jenna Kahn <jmkahn@hmc.edu>
2021-02-26 17:07:52 -0800

    reorganize fear_mice notebooks into atomic pipelines

--- cfl/util/fear_mice_functions.py
+++ cfl/util/fear_mice_functions.py
@@ -5,10 +5,64 @@ import nibabel as nib
 
 from cfl.util import brain_util as BU
 
+def select_from_dict(dict, keywords):
+    '''quick lil function to pull out only the arrays from a dict that match 
+    a keyword (used to select only KO mris or only BL, etc). keywords is a list of 
+    possible keywords (used to select based on an 'or' condition)
+    
+    Args: 
+        dict (dict): a dictionary with string keys and array values 
+        keywords (list of strings): list of strings that you want to find 
+            in the keys of dict to return 
+    Example: 
+        wts, wt_names = select_from_dict(all_HM_dir, ['WT'])
+    '''
+    selected = []
+    selected_names = []
+    for key in dict: 
+        for keyword in keywords: 
+            if keyword in key: 
+                selected.append(dict[key])
+                selected_names.append(key)
+    selected = np.array(selected)
+    return selected, selected_names
 
-def save_as_nifti(flat_array, fname, mri_dims, affine):
-    flat_array = np.nan_to_num(flat_array)
-    nifti_im = nib.Nifti1Image(BU.unflatten(flat_array, mri_dims), affine=affine)
+
+def get_global_values(): 
+    '''return mri dims, affine, and dir_labels (only
+    valid for this dataset and RPS orientation!
+
+    Intended usage: 
+        mri_dir, mri_dims, affine, dir_labels = fm.get_global_values()
+    
+    '''
+
+    mri_dir = r'PTSD_Data_Share\MEMRI_data'
+
+    # load one image to get its dimensions
+    img = BU.load_brain(os.path.join(mri_dir, "PTSD_KO_03_BL.nii"))
+    mri_dims = img.shape
+
+    # load one image to get affine
+    nib_img = nib.load(os.path.join(mri_dir, "PTSD_KO_03_BL.nii"))
+    affine = nib_img.affine
+
+    # specify labels for plot (note the labels below are specifically for RAS orientation)
+    dir_labels = { 'saggital' :   ['P', 'A', 'D', 'V'],
+                'coronal' :    [' ', ' ', ' ', ' '],
+                'horizontal' : ['L', 'R', 'A', 'P']} 
+
+    return mri_dir, mri_dims, affine, dir_labels
+
+
+def save_as_nifti(array, fname, mri_dims, affine):
+    '''if you don't pass in a flat array, then this still should work equally 
+    well. the unflatten just won't do anything'''
+    if len(array.shape)==1: #if the array is flattened
+        array = BU.unflatten(array, mri_dims)
+    assert array.shape == mri_dims, "Array is of the wrong shape"
+    array = np.nan_to_num(array) #change any NaNs in the array to 0s
+    nifti_im = nib.Nifti1Image(array, affine=affine)
     nib.save(nifti_im, fname)
 
 
