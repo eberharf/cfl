68ad002d1b356c81ee6814162f0de8345f4af850
Jenna Kahn <jmkahn@hmc.edu>
2021-04-21 15:58:48 -0700

    don't import everything when you import cfl (it's annoying)

--- cfl/__init__.py
+++ cfl/__init__.py
@@ -1,9 +1,2 @@
 # The root directory for the `cfl` package.
 # This folder contains all the functional code necessary to run `cfl`.
-import cfl.visualization_methods
-import cfl.cluster_methods
-import cfl.density_estimation_methods
-import cfl.util
-import cfl.dataset
-import cfl.experiment
-import cfl.block
