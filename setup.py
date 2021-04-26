1b66559b16b0a7018a289b995adb4205549f8e7b
Jenna Kahn <jmkahn@hmc.edu>
2021-04-26 14:26:07 -0700

    change numpy version (trying to get setup to work)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cfl', 
-    version='0.1.0', # version <1.0 indicates 'pre-release' 
+    version='0.1.1', # version <1.0 indicates 'pre-release' 
     description='Causal Feature Learning', #TODO: one sentence summary 
     url='https://github.com/eberharf/cfl',
     author='Jenna Kahn and Iman Wahle',
@@ -12,7 +12,7 @@ setup(
     package_dir={'': 'cfl'},
     python_requires = ">=3.7",  #TODO: good? it probably works with 3.6 too idk. guess that means do testing
     # install_requires=['tqdm','matplotlib' ,'tensorflow', 'keras', 'numpy~=1.19.2', 'scikit-learn'],
-    install_requires=['matplotlib' ,'tensorflow', 'keras', 'numpy~=1.9', 'scikit-learn'], 
+    install_requires=['matplotlib' ,'tensorflow', 'keras', 'numpy~=1.9.3', 'scikit-learn'], 
  
 
     classifiers=[
@@ -20,8 +20,8 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: Free for non-commercial use', #TODO: update if we change license
         'Natural Language :: English',
-        'Operating System  :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows :: Windows 10',
+        # 'Operating System  :: MacOS :: MacOS X',
+        # 'Operating System :: Microsoft :: Windows :: Windows 10',
         'Programming Language :: Python :: 3.7', # TODO: other versions? 
         'Programming Language :: Python :: 3.8', # TODO: other versions? 
         'Programming Language :: Python :: 3.9', # TODO: other versions? 
