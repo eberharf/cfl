85486e09400944388fec9ea9cfaeefd736cc55c9
Jenna Kahn <jmkahn@hmc.edu>
2021-05-03 12:31:13 -0700

    updates to requirements in setup for pip distribution - change tensorflow==2 to tensorflow>=2.4.0 (so that pip can find correct package) - increment version by 0.0.1 in __init__ - add some comments about how to build distribution in setup.py - change joblib requirement from ==0.16.0 to >= 0.16.0 (less rigid)

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 from setuptools import setup, find_packages
 import cfl 
 
-# TODO: right now, only cfl/ and visual_bars/ are packaged for installation 
-# eventually, we may want to package more things (docs and/or examples and/or
-# unit tests )
 VERSION = cfl.__version__
 AUTHORS = cfl.__author__
 CONTACT = 'iwahle@caltech.edu'
+
+# to create a new distribution for cfl 
+# run the commands: 
+#       python setup.py sdist bdist_wheel
+#       twine upload dist/*
+# SOURCE: see 
+# https://betterscientificsoftware.github.io/python-for-hpc/tutorials/python-pypi-packaging/#creating-a-python-package
+# for more information on how to package code for pyPI 
+
+# TODO: right now, only cfl/ and visual_bars/ are packaged for installation 
+# eventually, we may want to package more things (docs and/or examples and/or
+# unit tests )
 
 setup(
     name='cfl', 
     version=VERSION, 
     description='Causal Feature Learning (CFL) is an unsupervised algorithm designed to construct macro-variables from low-level data, while maintaining the causal relationships between these macro-variables. ', #TODO: one sentence summary 
-    long_description='See cfl.readthedocs.io for a full description'
+    long_description='See cfl.readthedocs.io for a full description', 
     url='https://github.com/eberharf/cfl',
     author=AUTHORS,
     author_email = CONTACT, 
@@ -22,12 +31,12 @@ setup(
     install_requires=[ #TODO: this list contains semi-redundant information with requirements.yml
         'tqdm',
         'matplotlib',
-        'tensorflow==2', 
+        'tensorflow>=2.4.0', 
         'numpy>=1.19.2', 
         'scikit-learn>=0.23',
         'jupyter', # for jupyter notebooks
         'ipykernel',
-        'joblib==0.16.0'
+        'joblib>=0.16.0'
         ], 
 
     classifiers=[
