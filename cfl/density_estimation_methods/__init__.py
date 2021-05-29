50cca3c5b2d6148e7ffb2dbf90afbec14db47ada
Jenna Kahn <jmkahn@hmc.edu>
2021-05-28 17:30:00 -0700

    create __all__ variable in density_estimation_methods
    Single-source list of all CDEs in the density_estimation_methods folder

--- cfl/density_estimation_methods/__init__.py
+++ cfl/density_estimation_methods/__init__.py
@@ -3,3 +3,13 @@ from cfl.density_estimation_methods.condExpCNN3D import CondExpCNN3D
 from cfl.density_estimation_methods.condExpKC import CondExpKC
 from cfl.density_estimation_methods.condExpMod import CondExpMod
 from cfl.density_estimation_methods.condExpVB import CondExpVB
+
+# if a new density estimation method is created, add it to this list 
+__all__ = [CondExpCNN, 
+           CondExpMod, 
+           CondExpCNN3D, 
+           CondExpVB, 
+           CondExpKC
+          ]
+
+
