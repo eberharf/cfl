700b6a6766ab5f3c94a87fa7e01c183b4d831f62
Jenna Kahn <jmkahn@hmc.edu>
2021-06-11 16:26:04 -0700

    import all CDEs in init
    Unclear why these got deleted but we do want them all so that when we
    import cfl.density_estimation_methods in the Experiment class, they all
    get imported

--- cfl/density_estimation_methods/__init__.py
+++ cfl/density_estimation_methods/__init__.py
@@ -1,1 +1,5 @@
-
+from cfl.density_estimation_methods.condExpCNN import CondExpCNN
+from cfl.density_estimation_methods.condExpCNN3D import CondExpCNN3D
+from cfl.density_estimation_methods.condExpKC import CondExpKC
+from cfl.density_estimation_methods.condExpMod import CondExpMod
+from cfl.density_estimation_methods.condExpVB import CondExpVB 
