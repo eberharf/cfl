c267e8e0deef054ff17679ac1a6ea6740af7fe58
Iman Wahle <imanwahle@gmail.com>
2021-06-15 15:39:44 -0700

    Try different dataset sizes

--- testing/test_intervention_rec.py
+++ testing/test_intervention_rec.py
@@ -36,7 +36,7 @@ CLUSTER_PARAMS = {'x_model' : KMeans(n_clusters=4),
 
 def generate_vb_data():
     # create a visual bars data set 
-    n_samples = 10000
+    n_samples = 5000
     noise_lvl = 0.03
     im_shape = (10, 10)
     random_seed = 143
