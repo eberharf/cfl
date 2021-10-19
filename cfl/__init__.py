96419784efe47e940355657a7960c51244086889
Iman Wahle <imanwahle@gmail.com>
2021-10-18 17:22:48 -0700

    Increment version

--- cfl/__init__.py
+++ cfl/__init__.py
@@ -1,6 +1,6 @@
 # The root directory for the `cfl` package.
 # This folder contains all the functional code necessary to run `cfl`.
 
-__version__ = '1.0.1' 
+__version__ = '1.0.2' 
 __author__= 'Jenna Kahn and Iman Wahle'
 __credits__= '2021, California Institute of Technology'
