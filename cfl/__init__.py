57a1a252201f2c1cf5c90afd3f9c6aa34e5b22ef
Iman Wahle <imanwahle@gmail.com>
2022-02-17 11:47:24 -0800

    Import classes automatically in cfl dir

--- cfl/__init__.py
+++ cfl/__init__.py
@@ -4,3 +4,7 @@
 __version__ = '1.1.0'
 __author__ = 'Jenna Kahn and Iman Wahle'
 __credits__ = '2021, California Institute of Technology'
+
+from cfl.experiment import Experiment
+from cfl.block import Block
+from cfl.dataset import Dataset
