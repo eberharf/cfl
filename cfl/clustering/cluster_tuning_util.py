fa2ce7cc51b60ebe6a09ef57a147868bc8744109
iwahle <imanwahle@gmail.com>
2026-05-05 16:58:23 -0400

    Clean up visualizations, cv code

--- cfl/clustering/cluster_tuning_util.py
+++ cfl/clustering/cluster_tuning_util.py
@@ -17,13 +17,14 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression as LR
 from sklearn.model_selection import ParameterGrid
 from sklearn.cluster import *
+from sklearn.model_selection import cross_val_score, ShuffleSplit
 
 from cfl.util.data_processing import one_hot_encode
 from tqdm import tqdm
 import copy
 
 # set font size for all plots
-font = {'size' : 16}
+font = {'size' : 8}
 import matplotlib
 matplotlib.rc('font', **font)
 
@@ -39,18 +40,15 @@ def _score(true, pred):
     '''
     return np.mean(np.power(true-pred, 2))
 
-def compute_predictive_error(Xlr, Ylr, n_iter=100):
+def compute_predictive_error(Xlr, Ylr):
     ''' 
-    Fits a linear model to a randomly selected subset of data and evalutes
-    this model on the remaining subset of data n_iter times, then returns
-    the average error over these n_iter runs.
+    Fits and evaluates a linear model to 10 folds of data, then returns
+    the average error.
     
     Arguments:
         Xlr (np.ndarray) : array of cluster assignments of size (n_samples,)
         Ylr (np.ndarray) : array of original data points used for clustering,
             of size (n_samples, n_features)
-        n_iter (int) : number of times to retrain and evaluate model. Defaults
-            to 100. 
     Returns:
         np.float : mean error across n_iter runs
     '''
@@ -60,24 +58,11 @@ def compute_predictive_error(Xlr, Ylr, n_iter=100):
     if Ylr.ndim == 1:
         Ylr = np.expand_dims(Ylr, -1)
 
-    n_samples = Xlr.shape[0]
-    errs = np.zeros((n_iter,))
-    for ni in range(n_iter):
-        try:
-            # choose samples to withhold
-            in_sample_idx = np.zeros((n_samples,))
-            in_sample_idx[np.random.choice(
-                n_samples, int(n_samples*0.95), replace=False)] = 1
-            in_sample_idx = in_sample_idx.astype(bool)
-
-            # get predictive error
-            reg = LR().fit(Xlr[in_sample_idx], Ylr[in_sample_idx])
-            pred = reg.predict(Xlr[~in_sample_idx])
-            errs[ni] = _score(Ylr[~in_sample_idx], pred)
-        except:
-            errs[ni] = np.nan
-    return np.mean(errs)
-
+    clf = LR()
+    cv = ShuffleSplit(n_splits=10, random_state=0)
+    errs = -cross_val_score(clf, Xlr, Ylr, cv=10,
+                            scoring='neg_mean_squared_error')
+    return np.nanmean(errs), np.nanstd(errs)
 
 def get_parameter_combinations(param_ranges):
     '''
@@ -95,12 +80,13 @@ def get_parameter_combinations(param_ranges):
     return param_combos
 
 
-def visualize_errors(errs, params_list, params_to_tune):
+def visualize_errors(errs, stds, params_list, params_to_tune, vis_thresh=1e4):
     '''
     Visualizes the errors computed for every parameter combination. 
 
     Arguments:
         errs (np.ndarray) : array of error for every parameter combination
+        stds (np.ndarray) : array of standard deviation for every parameter combination
         params_list (list): list of dicts of all parameter combinations as 
             given by `get_parameter_combinations`
         params_to_tune (dict) : original dict of parameters to iterate over.
@@ -125,23 +111,24 @@ def visualize_errors(errs, params_list, params_to_tune):
             idx = np.where(k0_vals == k_ord)
             shaped_errs[k_ord_i] = errs[idx[0]]
 
+        np.save('clusterer_grid_search.npy', shaped_errs)
+
         # mask any outliers
         shaped_errs = np.ma.masked_where(
-            shaped_errs > np.median(shaped_errs)*10, shaped_errs)
-        # shaped_errs[shaped_errs > np.median(shaped_errs)*10] = np.nan
-        # shaped_errs = np.ma.masked_invalid(shaped_errs)
-        # plt.get_cmap().set_bad(color='w', alpha=1.)
-        # cmap = copy.copy(plt.get_cmap()).set_bad(color='w', alpha=1.)
+            shaped_errs > vis_thresh, shaped_errs)
+        shaped_errs[shaped_errs > vis_thresh] = np.nan
+        shaped_errs = np.ma.masked_invalid(shaped_errs)
+        plt.get_cmap().set_bad(color='w', alpha=1.)
+        cmap = copy.copy(plt.get_cmap()).set_bad(color='w', alpha=1.)
 
-
+        print(shaped_errs)
         # 1D line plot
-        fig, ax = plt.subplots()
+        fig, ax = plt.subplots(figsize=(3,3))
         ax.plot(params_to_tune[k0], shaped_errs)
-        ax.set_xticks(params_to_tune[k0])
-        ax.set_xticklabels(params_to_tune[k0])
+        ax.fill_between(params_to_tune[k0], shaped_errs-stds, shaped_errs+stds, alpha=0.5)
+        ax.set_xlim([params_to_tune[k0][0], params_to_tune[k0][-1]])
         ax.set_xlabel(k0)
-        ax.set_ylabel('Error')
-        ax.set_title('Prediction Error (MSE)')
+        ax.set_ylabel('MSE')
         plt.tight_layout()
         plt.savefig('clusterer_grid_search.png', dpi=300)
         return fig
@@ -248,28 +235,44 @@ def tune(data_to_cluster, model_name, model_params, user_input):
         (matplotlib.pyplot.Figure) : figure displaying tuning errors
         (np.ndarray) : array of errors for each hyperparameter combination
         (param_combos) : list of dictionaries of each hyperparameter combination
+
+    Notes:
+        * TODO: handle -1 case in DBSCAN
     '''
 
     # get list of parameter combos to optimize over
     param_combos = get_parameter_combinations(model_params)
     errs = np.zeros((len(param_combos),))
+    stds = np.zeros((len(param_combos),))
     print('Beginning clusterer tuning')
     for ci, cur_params in tqdm(enumerate(param_combos.copy())):
         # create model with given params
         tmp_model = eval(model_name)(**cur_params)
 
-        # do clustering
-        tmp_model.fit(data_to_cluster)
-        lbls = tmp_model.labels_
-
-        # compute error measure (error in predicting pyx from Xbarhat)
-        # TODO: handle -1 case in DBSCAN
-        errs[ci] = compute_predictive_error(
-            one_hot_encode(lbls, np.unique(lbls)), data_to_cluster)
+        lbls = tmp_model.fit_predict(data_to_cluster)
+        good_clusters = True
+        if model_name=='KMeans':
+            # check if any clusters have no members
+            n_clusters = cur_params['n_clusters']
+            n_members = [np.sum(lbls==i) for i in range(n_clusters)]
+            if np.min(n_members) < 1:
+                good_clusters = False
+        
+        if good_clusters:
+            # compute error measure (error in predicting pyx from Xbarhat)
+            errs[ci], stds[ci] = compute_predictive_error(
+                one_hot_encode(lbls, np.unique(lbls)), data_to_cluster)
+        else:
+            errs[ci] = np.nan
+            stds[ci] = np.nan
 
     # visualize errors and solicit user input
-    fig = visualize_errors(errs, param_combos, model_params)
-    suggested_params = param_combos[suggest_elbow_idx(errs)]
+    fig = visualize_errors(errs, stds, param_combos, model_params)
+    try:
+        suggested_params = param_combos[suggest_elbow_idx(errs)]
+    except:
+        suggested_params = param_combos[np.nanargmin(errs)]
+        
     if user_input:
         chosen_params = get_user_params(suggested_params)
     else:
